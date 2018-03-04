cc7675d882e03bacf3a47e0b5fd451a19c90ff4d
bwielk <bwielk@gmail.com>
2018-03-04 18:39:49 +0100

    Immutability implemented

--- src/adventure/Location.java
+++ src/adventure/Location.java
@@ -10,7 +10,7 @@ public class Location {
 	public Location(int locationID, String description, Map<String, Integer> exits){
 		this.locationID = locationID;
 		this.description = description;
-		this.exits = exits;
+		this.exits = new HashMap<String, Integer>(exits);
 		exits.put("Q", 0);
 	}
 
